5f68a1fe9b6678b51db34a821d78fadde03cb68e
Primica <artbrun@icloud.com>
2025-08-06 01:03:11 +0200

    Mise à jour de l'attribut AssemblyInformationalVersion dans Graphqlite pour refléter un nouveau hash. Modification du cache des informations d'assemblage en conséquence.

--- obj/Debug/net9.0/Graphqlite.AssemblyInfo.cs
+++ obj/Debug/net9.0/Graphqlite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Graphqlite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beb02c0042a597ec8fd1eb976a885839aeab79ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9894715f64d2d079a39c2948d1b2c532d443a4e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Graphqlite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Graphqlite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
